d2e1b744a52fa8323dd92bf428d7b34a65cf4e99
immaonsombi <immanuel.onsombi@strathmore.edu>
2023-11-03 10:05:55 +0300

    Create Arrays.php

---
+++ Arrays.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+$n = intval(trim(fgets(STDIN)));
+
+$arr_temp = rtrim(fgets(STDIN));
+
+$arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+print strrev($arr_temp);
+
+
+
+<?php
+
+
+
+$n = intval(trim(fgets(STDIN)));
+
+$arr_temp = rtrim(fgets(STDIN));
+
+$arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$reversedArr = array_reverse($arr);
+
+print implode(' ',$reversedArr );
